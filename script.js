12e629decbbed5e78b117bdb608fdd73d839420b
HARSHITA <93899633+Harshyis@users.noreply.github.com>
2022-05-22 17:02:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-var password = document.querySelector(".password")
-function whenchecked() {
-    if (password.type == "password") {
-      password.type = "text"
-    }
-    else (
-      password.type = "password"
-    )
-  }
+
